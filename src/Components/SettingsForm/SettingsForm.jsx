7279eb4604d11bf66bf1ec6e00386da708d556da
Kenny W. Lino <kennywlino@gmail.com>
2023-01-11 10:31:02 -0800

    Adding more Mantine styling for Settings Form component.

--- src/Components/SettingsForm/SettingsForm.jsx
+++ src/Components/SettingsForm/SettingsForm.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { SettingsContext } from "../../Context/Settings/Settings";
-import { createStyles, Grid, NumberInput, Switch } from "@mantine/core";
+import { Button, createStyles, Grid, NumberInput, Switch, TextInput } from "@mantine/core";
 import { IconSettings } from '@tabler/icons'
 
 const useStyles = createStyles((theme) => ({
@@ -9,6 +9,10 @@ const useStyles = createStyles((theme) => ({
         color: theme.colors.gray[0],
         width: '80%',
         margin: 'auto',
+        fontSize: theme.fontSizes.lg,
+        padding: theme.spacing.md,
+        marginBottom: theme.spacing.md,
+        marginTop: theme.spacing.md,
     }
 }));
 
@@ -17,7 +21,6 @@ const useStyles = createStyles((theme) => ({
 
 const SettingsForm = () => {
 
-    const { classes } = useStyles();
     
     const { 
         showCount,
@@ -26,9 +29,15 @@ const SettingsForm = () => {
         setShowCount,
         setSortKey,
         setShowCompleted,
-
+        
     } = useContext(SettingsContext);
     
+    const { classes } = useStyles();
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //setShowUpdatedSettings(true);
+    }
     
     return (
         <>
@@ -36,17 +45,24 @@ const SettingsForm = () => {
 
         <Grid style={{width: '80%', margin: 'auto'}}>
             <Grid.Col xs={12} sm={6}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h3>Update Settings</h3>
                 <Switch 
-                    label="Show completed"
+                    label="Show completed ToDos"
                     checked={showCompleted}
                     onChange={((e) => setShowCompleted(e.currentTarget.checked))}
                 />
                 <NumberInput
-                    defaultValue={showCount} 
+                    value={showCount} 
                     label="Items Per Page"
+                    onChange={((val) => setShowCount(val))}
                 />
+                <TextInput
+                    placeholder={sortKey}
+                    onChange={(e) => setSortKey(e.target.value)}
+                    label="Sort key"
+                />
+                <Button type="submit">Show New Settings</Button>
             </form>
             </Grid.Col>
             <Grid.Col xs={12} sm={6}>
