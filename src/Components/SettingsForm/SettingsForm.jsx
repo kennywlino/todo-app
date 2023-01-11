405f1f2fd934f0a8715d624494736eed145eec11
Kenny W. Lino <kennywlino@gmail.com>
2023-01-11 10:04:56 -0800

    Adding Mantine styling following code review.

--- src/Components/SettingsForm/SettingsForm.jsx
+++ src/Components/SettingsForm/SettingsForm.jsx
@@ -1,8 +1,24 @@
 import { useContext } from "react";
 import { SettingsContext } from "../../Context/Settings/Settings";
-import { NumberInput, Switch } from "@mantine/core";
+import { createStyles, Grid, NumberInput, Switch } from "@mantine/core";
+import { IconSettings } from '@tabler/icons'
+
+const useStyles = createStyles((theme) => ({
+    h1: {
+        backgroundColor: theme.colors.gray[8],
+        color: theme.colors.gray[0],
+        width: '80%',
+        margin: 'auto',
+    }
+}));
+
+
+
 
 const SettingsForm = () => {
+
+    const { classes } = useStyles();
+    
     const { 
         showCount,
         sortKey,
@@ -16,19 +32,28 @@ const SettingsForm = () => {
     
     return (
         <>
-        <h2>Manage Settings</h2>
-        <form onSubmit={() => {}}>
-            <h3>Update Settings</h3>
-            <Switch 
-                label="Show completed"
-                checked={showCompleted}
-                onChange={((e) => setShowCompleted(e.currentTarget.checked))}
-            />
-            <NumberInput
-                defaultValue={showCount} 
-                label="Items Per Page"
-            />
-        </form>
+        <h1 className={classes.h1}><IconSettings />Manage Settings</h1>
+
+        <Grid style={{width: '80%', margin: 'auto'}}>
+            <Grid.Col xs={12} sm={6}>
+            <form>
+                <h3>Update Settings</h3>
+                <Switch 
+                    label="Show completed"
+                    checked={showCompleted}
+                    onChange={((e) => setShowCompleted(e.currentTarget.checked))}
+                />
+                <NumberInput
+                    defaultValue={showCount} 
+                    label="Items Per Page"
+                />
+            </form>
+            </Grid.Col>
+            <Grid.Col xs={12} sm={6}>
+
+            </Grid.Col>
+
+        </Grid>
         </>
     );
 }
