e4b598a26fee777cfe3948a5d9795d98e841f140
Kenny W. Lino <kennywlino@gmail.com>
2023-01-11 09:41:50 -0800

    Added form elements to SettingsForm.

--- src/Components/SettingsForm/SettingsForm.jsx
+++ src/Components/SettingsForm/SettingsForm.jsx
@@ -1,7 +1,34 @@
+import { useContext } from "react";
+import { SettingsContext } from "../../Context/Settings/Settings";
+import { NumberInput, Switch } from "@mantine/core";
+
 const SettingsForm = () => {
+    const { 
+        showCount,
+        sortKey,
+        showCompleted,
+        setShowCount,
+        setSortKey,
+        setShowCompleted,
+
+    } = useContext(SettingsContext);
+    
+    
     return (
         <>
-        <h2>Settings Form</h2>
+        <h2>Manage Settings</h2>
+        <form onSubmit={() => {}}>
+            <h3>Update Settings</h3>
+            <Switch 
+                label="Show completed"
+                checked={showCompleted}
+                onChange={((e) => setShowCompleted(e.currentTarget.checked))}
+            />
+            <NumberInput
+                defaultValue={showCount} 
+                label="Items Per Page"
+            />
+        </form>
         </>
     );
 }
