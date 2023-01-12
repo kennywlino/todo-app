e35d993fc0e4ea5fb40703209515ddcdb5b41c2b
Kenny W. Lino <kennywlino@gmail.com>
2023-01-11 21:23:30 -0800

    Fixed styling for login.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,8 @@
-import { Text, createStyles, Group } from '@mantine/core';
+import { Button, TextInput, createStyles, Group } from '@mantine/core';
+import { useContext, useState } from 'react';
 import { Link } from "react-router-dom";
+import { AuthContext } from '../../Context/Auth';
+import './Header.scss'
 
 const useStyles = createStyles((theme) => ({
     header: {
@@ -10,17 +13,50 @@ const useStyles = createStyles((theme) => ({
         fontSize: theme.fontSizes.md,
         color: theme.colors.gray[0],
         textDecoration: 'none'
+    },
+    textInput: {
+        label: {
+            color: theme.colors.gray[0],
+        }
     }
 }));
 
 const Header = () => {
     const { classes } = useStyles();
 
+    const { login, logout, user } = useContext(AuthContext);
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        login(username, password);
+    }
+
     return (
         <header className={classes.header}>
-            <Group>
-                <Link className={classes.link} to="/">Home</Link>
-                <Link className={classes.link} to="/settings">Settings</Link>
+            <Group position="apart">
+                <Group>
+                    <Link className={classes.link} to="/">Home</Link>
+                    <Link className={classes.link} to="/settings">Settings</Link>
+                </Group>
+                <Group>
+                    <form onSubmit={handleLogin} className='login'>
+                        <TextInput 
+                            placeholder="username"
+                            className={classes.textInput}
+                            onChange={(e) => setUsername(e.target.value)}
+                            />
+                        <TextInput 
+                            placeholder="password"
+                            className={classes.textInput}
+                            onChange={(e) => setPassword(e.target.value)}
+                            />
+                        <Button color="dark">Login</Button>
+                    </form>
+                </Group>
             </Group>
         </header>
     )
